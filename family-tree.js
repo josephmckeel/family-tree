312b88618d9b84c4fc1ed8c7c79ee70b194d1604
Taylor McKeel <josephmckeel1993@gmail.com>
2020-04-27 18:06:54 -0400

    Final
    I got the log to work, but wasnt passing the test for it since it wasnt returning the array but the joined version of it

--- family-tree.js
+++ family-tree.js
@@ -1,5 +1,55 @@
 class FamilyTree {
-  constructor () {}
+  constructor(first) {
+    if (!first || typeof (first) !== 'string') {
+      throw new Error('Need name')
+    }
+    this.value = first,
+      this.number = 1,
+      this.children = []
+  }
+
+  familySize() {
+    let count = 1 + this.children.length
+    return count
+  }
+
+  findMember(name) {
+    if (name === this.value) {
+      return this
+    }
+    for (let i = 0; i < this.children.length; i++) {
+      if (this.children[i].value === name) {
+        return this.children[i]
+      }
+    }
+    for (let i = 0; i < this.children.length; i++) {
+      this.children[i].familySize()
+    }
+
+  }
+
+  log() {
+    let node = 1
+    let final = [`-${this.value}\n`]
+    this.children.map(child => {
+      let currentStr = `--${child.value}\n`
+      final.push(currentStr)
+      if (child.children.length !== 0) {
+        child.children.map(grand => {
+          let currStr = `---${grand.value}\n`
+          final.push(currStr)
+        })
+      }
+    })
+    return final.join('')
+  }
+
+  insert(name) {
+    this.size++
+    this.children.push(new FamilyTree(name))
+  }
 }
 
 module.exports = FamilyTree;
+
+
